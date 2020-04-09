306eb7591fe42d71ff221a8efa9e87059ecaa3f4
JohnRambo69 <50015050+JohnRambo69@users.noreply.github.com>
2020-04-09 12:04:01 +0200

    init

---
+++ spring-ecommerce/src/main/java/com/rambo/ecommerce/entity/Product.java
@@ -0,0 +1,12 @@
+package com.rambo.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+}
